499f79b831a3cdcf2e61a49d29db418de365b9b2
eastend-street <ss_1322irf@icloud.com>
2019-05-23 13:58:33 -0700

    Add cylinder.py

--- course_avreage.py
+++ course_avreage.py
@@ -2,7 +2,7 @@ intro = int(input('Intro: '))
 algorithm = int(input('Algorithm: '))
 java = int(input('Java: '))
 
-average = (intro + algorithm + java) / 3
+average = (intro + algorithm + java) // 3
 
 if average >= 70:
     print(f'Your average is {average}. Pass')
---
+++ cylinder.py
@@ -0,0 +1,8 @@
+import math
+
+radius = int(input('Radius: '))
+height = int(input('Height: '))
+
+area = (math.pi * radius * radius) * height
+
+print(f'The area is {area}.')
